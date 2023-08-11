cffa4459af10047c52b5dfeb4805458d54402a8a
gphics <97003589+gphics@users.noreply.github.com>
2023-08-11 01:48:41 +0100

    note

--- backend/Config/webPush.js
+++ backend/Config/webPush.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const webPush = require("web-push");
-const privateKey = "1IGcvU21Vp8heu9pKD2-ODFXDAJJYkqvDeXlZpuSntc";
-const publicKey =
-  "BP6wc7uBiJ-S1LbydpZM7-WRsh2X1dUqcnOWL-TqCgiB9UblBfT0XBrTt2JzrjvAKZtiobBIDJCRsyZzcAUMWsw";
+// const privateKey = "1IGcvU21Vp8heu9pKD2-ODFXDAJJYkqvDeXlZpuSntc";
+// const publicKey =
+//   "BP6wc7uBiJ-S1LbydpZM7-WRsh2X1dUqcnOWL-TqCgiB9UblBfT0XBrTt2JzrjvAKZtiobBIDJCRsyZzcAUMWsw";
 // const {encode, decode} = require("url-safe-base64")
 // const key = decode(privateKey)
 
 // console.log(key);
+const {privateKey, publicKey} = webPush.generateVAPIDKeys()
 webPush.setVapidDetails(
   "mailto:abdulbasitabdulakeem234@gmail.com",
   publicKey,
