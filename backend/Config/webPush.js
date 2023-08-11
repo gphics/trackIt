19c3cfd353bcf1ab731f8e747ae526a5d26a0c0e
gphics <97003589+gphics@users.noreply.github.com>
2023-08-11 01:33:17 +0100

    still on the notification

--- backend/Config/webPush.js
+++ backend/Config/webPush.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const webPush = require("web-push");
-const privateKey = process.env.WEB_PUSH_PRIVATE_KEY;
-const publicKey = process.env.WEB_PUSH_PUBLIC_KEY;
+const privateKey = "1IGcvU21Vp8heu9pKD2-ODFXDAJJYkqvDeXlZpuSntc";
+const publicKey =
+  "BP6wc7uBiJ-S1LbydpZM7-WRsh2X1dUqcnOWL-TqCgiB9UblBfT0XBrTt2JzrjvAKZtiobBIDJCRsyZzcAUMWsw";
+// const {encode, decode} = require("url-safe-base64")
+// const key = decode(privateKey)
 
-
+// console.log(key);
 webPush.setVapidDetails(
   "mailto:abdulbasitabdulakeem234@gmail.com",
   publicKey,
