cc57666fa4891f5a29a2e51d801b0e4db6896888
gphics <97003589+gphics@users.noreply.github.com>
2023-08-19 23:00:05 +0100

    created the navbar

--- client/src/sw.js
+++ client/src/sw.js
@@ -1,8 +1,11 @@
+import { precacheAndRoute } from "workbox-precaching";
 self.addEventListener("install", (e) => {
-    console.log("I am installing", e)
-})
+  console.log("I am installing", e);
+});
 
 self.addEventListener("push", (e) => {
-  const { title, body } = e.data
-  self.registration.sendNotification(title, {body})
-})
+  const { title, body } = e.data;
+  self.registration.sendNotification(title, { body });
+});
+
+precacheAndRoute(self.__WB_MANIFEST);
